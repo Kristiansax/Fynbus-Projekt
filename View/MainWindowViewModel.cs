15abfe390ff85f03309c3b67912307374f4dbfe3
Petter009 <pctwinengine@hotmail.com>
2017-03-30 10:55:53 +0200

    Comments

--- View/MainWindowViewModel.cs
+++ View/MainWindowViewModel.cs
@@ -13,18 +13,29 @@ namespace View
 
         public bool ImportDone { get; set; }
         public bool SelectionDone { get; set; }
-
+        
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public MainWindowViewModel()
         {
             iOController = new IOController();
             selectionController = new SelectionController();
             ImportDone = false;
         }
-
+        
+        /// <summary>
+        /// Calls the IOController to handle the importing of data from the two selected files.
+        /// </summary>
         public void ImportCSV(string masterDataFilepath, string routeNumberFilepath)
         {
             iOController.InitializeImport(masterDataFilepath, routeNumberFilepath);
         }
+        
+        /// <summary>
+        /// Enables you to choose a CSV file on your pc that this method will return.
+        /// Multiselecting files has been disabled to ensure you only select one file, as this program only processes 1 file in that particular slot.
+        /// </summary>
         public string ChooseCSVFile()
         {
             string filename = "Ingen fil er valgt";
@@ -38,7 +49,10 @@ namespace View
             }
             return filename;
         }
-
+        
+        /// <summary>
+        /// Saves the result as a CSV file with the information required to calling a company, if a winner has been found.
+        /// </summary>
         public void SaveCSVCallFile()
         {
             if (SelectionDone == true)
@@ -58,6 +72,10 @@ namespace View
                 MessageBox.Show("Du har ikke udvalgt vinderne endnu.. Kør Udvælgelse først!");
             }
         }
+        
+        /// <summary>
+        /// Saves the result as a CSV file and in a format used for publishing, if a winner has been found.
+        /// </summary>
         public void SaveCSVPublishFile()
         {
             if (SelectionDone == true)
@@ -78,6 +96,7 @@ namespace View
                 MessageBox.Show("Du har ikke udvalgt vinderne endnu.. Kør Udvælgelse først!");
             }
         }
+        
         public void InitializeSelection()
         {
             if (ImportDone)
